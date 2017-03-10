4a19ae2aeea16e0007c4af2fc056a4114b34acfb
fendoudexiaohao <1344911871@qq.com>
2017-03-10 20:35:30 +0800

    类似qq的底部导航栏

---
+++ app/src/main/java/songzhihao/bwei/com/daohang/Entrepreneur_fragment.java
@@ -0,0 +1,22 @@
+package songzhihao.bwei.com.daohang;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class Entrepreneur_fragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.entrepreneur_fragment, container, false);
+    }
+
+}
